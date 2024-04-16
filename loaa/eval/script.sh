25899772705b9c5c33c55bd3afd99ec9fe69e353
Kthyeon <potter32@kaist.ac.kr>
2024-04-16 16:04:09 +0900

    Add - scripts for evaluation of head accuracy

---
+++ loaa/eval/script.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=7 python ./loaa/eval/heads_accuracy.py --loaa_path ./loaa_6_width_0.25_self_no_shortcut/loaa_lm_head.safetensors --loaa_num_heads 6 --loaa_width 0.25 --save_dir ./data --steps 2048 --model_name_or_path lmsys/vicuna-7b-v1.3 --data_path question.jsonl
+
+python ./loaa/eval/gen_results.py --accuracy-path ./data/loaa_6_width_0.25_self_no_shortcut_heads_accuracy.pt --output-path ./data
